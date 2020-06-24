99ec08bcc76ebc3ae8e6423448fa7cdad8c980f9
Abhijit Biswal <biswalabhijit04@gmail.com>
2020-06-24 12:14:17 +0530

    Added stats for other tabs

--- js/pipboy.app.js
+++ js/pipboy.app.js
@@ -25,6 +25,42 @@ $(document).ready(function(){
         "value": 144
     }
   ];
+  
+  var junk = [
+    {
+        "name":"baseball",
+        "weight":0.5,
+        "value":4
+    },
+    {
+        "name":"desk_fan",
+        "weight":3,
+        "value":4
+    },
+    {
+        "name":"ladle",
+        "weight":0.5,
+        "value":2
+    }
+    ];
+
+    var ammo = [
+    {
+        "name":"5.56",
+        "weight":0,
+        "value":2
+    },
+    {
+        "name":"5mm",
+        "weight":0,
+        "value":1
+    },
+    {
+        "name":"shotgun",
+        "weight":0,
+        "value":3
+    }
+  ];
 
   $('.item-list a').on('click',function(e){
     $('.item-list a').removeClass('active');
@@ -46,6 +82,35 @@ $(document).ready(function(){
         container.find('.range').html(weapons[item].range);
         container.find('.weight').html(weapons[item].weight);
         container.find('.value').html(weapons[item].value);
+      }
+    }
+  });
+  $('.item-list a').on('mouseenter', function(e){
+    var current_item=$(e.currentTarget).attr('class');
+    console.log(current_item);
+
+    for(item in junk){
+      if(junk[item].name == current_item){
+        console.log(junk[item]);
+
+        var container=$('.item-stats');
+        container.find('.weight').html(junk[item].weight);
+        container.find('.value').html(junk[item].value);
+      }
+    }
+  });
+
+  $('.item-list a').on('mouseenter', function(e){
+    var current_item=$(e.currentTarget).attr('class');
+    console.log(current_item);
+
+    for(item in ammo){
+      if(ammo[item].name == current_item){
+        console.log(ammo[item]);
+
+        var container=$('.item-stats');
+        container.find('.weight').html(ammo[item].weight);
+        container.find('.value').html(ammo[item].value);
       }
     }
   });
